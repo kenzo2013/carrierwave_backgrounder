61a6af8a7ba427bdb67ee8351c5f9e87dca29f65
Eric <kkenzo2007@yahoo.fr>
2018-12-10 15:42:36 +0100

    Hack: Getting cache directory to work on s3.

--- lib/backgrounder/workers/store_asset_mixin.rb
+++ lib/backgrounder/workers/store_asset_mixin.rb
@@ -34,11 +34,12 @@ module CarrierWave
       def store_directories(record)
         asset, asset_tmp = record.send(:"#{column}"), record.send(:"#{column}_tmp")
         cache_directory  = File.expand_path(asset.cache_dir, asset.root)
-        # @cache_path      = File.join(cache_directory, asset_tmp)
         if Rails.env.production?
           @cache_path      = open("https://wire-files.s3.amazonaws.com/#{asset_tmp}")
         elsif Rails.env.staging?
           @cache_path      = open("https://wire-files-staging.s3.amazonaws.com/#{asset_tmp}")
+        else
+          @cache_path      = File.join(cache_directory, asset_tmp)
         end
         @tmp_directory   = File.join(cache_directory, asset_tmp.split("/").first)
       end
